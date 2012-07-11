c06f90a453b3947036061ba9f5beb6353998c347
Nathan <nathanlubchenco@Nathans-MacBook-Air.local>
2012-07-11 13:46:56 -0600

    Replaced tutorial queries with 5 different queries

--- js/app/config/demo-queries.js
+++ js/app/config/demo-queries.js
@@ -5,118 +5,23 @@ define([
 function(utils) {
     var values = [{
 
-        name : "00 Income By State",
-        code : 'users := //users\nincomeByState := forall \'location\n\t{location: \'location, mean : mean(users.income where users.location = \'location)}\nincomeByState where incomeByState.mean = max(incomeByState.mean)'
+        name : "Income By State",
+        code : '--This query will return the State(s) with the highest average income\n\nusers := //users --assigns the user data to a variable called "users"\n\nincomeByState := forall \'location\n\t\t{location: \'location, mean : mean(users.income where users.location = \'location)}\n--These two lines determine the average income of each state by using the mean function within a forall statement\n\nincomeByState where incomeByState.mean = max(incomeByState.mean)\n--returns the results filtered by a \'where\' condition, in this case: States that have the max average income'
     }, {
-        name : "01 Json - Object",
-        code : '{name: "John", age: 29, gender: "male"}'
+        name : "Taxes By User",
+        code : '--This query creates a tax variable and adds it to the orders dataset, it then uses this variable to return the users who pay above some threshold in taxes\n\norders := //orders --assigns the orders data to a variable called \"orders\"\nthreshold := 1 --creates a variable called threshold, set to 1\n\nordersWithTax := orders with {tax: orders.taxRate*orders.total} --adds a new variable called tax to the orders data set\n\ntaxByUser := forall \'userId\n\t\t{userId : \'userId, sum: sum(ordersWithTax.tax where ordersWithTax.userId = \'userId)}\n--These two lines sum the tax for all users\n\ntaxByUser where taxByUser.sum > threshold --returns the results filtered by those greater than the threshold' 
     }, {
-        name : "02 Json - Value",
-        code : 'true'
+        name : "Conversion Rate",
+        code : '--This query returns conversions per hour from clicks data\n--Check out the chart option for displaying data\n\nclicks := //clicks --assigns clicks data to a variable called \"clicks\"\nconversionRate := 0.045 --creates a conversion rate variable that can be easily modified\n\nclicksWithHour := clicks with {hour: (std::time::hourOfDay(clicks.timeString))}\n--adds an hour variable created using the hourOfDay function to the clicks data and stores it in clicksWithHour\n\nconversionsPerHour(\'hour) :=\n{hour: \'hour, conversions: count(clicksWithHour where clicksWithHour.hour = \'hour)*conversionRate}\n\nconversionsPerHour--returns the results unfiltered'
     }, {
-        name : "03 Json - String",
-        code : '"hello world"'
+        name : "Simple Filtering",
+        code : '--This query is designed to show some basic filtering using the dot operator and the where command\n--In order to see the results of the different levels of filtering uncomment a line and hit shift+enter to run the query, to see a different line, recomment the previous line using -- and uncomment another.  Note that no results will appear without uncommenting one of the lines.\n\ntransaction := //transaction\n\n--transaction --returns all the data\n--transaction.timestamp --returns just the timestamp data\n--transaction.timestamp.time--filters even further and returns just the time set from transaction.time\n--transaction.timestamp.time where transaction.timestamp.date > 3 --filters the previous line using a where filter to return only the results where the data is greater than 3'
     },
-    // NUMBERS
+    
     {
-        name : "04 Numbers - Sum",
-        code : '5 + 2'
-    }, {
-        name : "05 Numbers - Multiplication",
-        code : '8 * 2'
-    },
-    // BOOLEANS
-    {
-        name : "06 Booleans - Comparison",
-        code : '5 > 2'
-    }, {
-        name : "07 Booleans - Inequality",
-        code : '"foo" != "foo"'
-    },
-    // VARIABLES
-    {
-        name : "08 Variables - Total",
-        code : 'total := 2 + 1\ntotal * 3'
-    }, {
-        name : "09 Variables - Square",
-        code : 'num := 4\nsquare := num * num\nsquare - 1'
-    },
-    // LOADING DATA
-    {
-        name : "10 Loading Data",
-        code : '//payments'
-    },
-    // FILTERED DESCENT
-    {
-        name : "11 Filtered Descent - Date",
-        code : 'payments := //payments\npayments.date'
-    }, {
-        name : "12 Filtered Descent - Index",
-        code : 'payments := //payments\npayments.recipients[0]'
-    },
-    // REDUCTIONS
-    {
-        name : "13 Reductions - Count",
-        code : 'count(//payments)'
-    }, {
-        name : "14 Reductions - Mean",
-        code : 'mean(//payments.amount)'
-    }, {
-        name : "15 Reductions - Sum",
-        code : 'sum(//payments.amount)'
-    },
-    // IDENTITY MATCHING
-    {
-        name : "16 Identity Matching",
-        code : 'orders := //orders\norders.subTotal + orders.subTotal * orders.taxRate + orders.shipping + orders.handling'
-    },
-    // VALUES
-    {
-        name : "17 Values",
-        code : 'payments := //payments\npayments.amount * 0.10'
-    },
-    // FILTERING
-    {
-        name : "18 Filtering - Payments",
-        code : 'payments := //payments\ncount(payments where payments.amount > 10.00)'
-    }, {
-        name : "19 Filtering - Users",
-        code : 'users := //users\nsegment := users.age > 19 & users.age < 53 & users.income > 60000\ncount(users where segment)'
-    },
-    // CHAINING
-    {
-        name : "20 Chaining",
-        code : 'pageViews := //pageViews\nbound := 1.5 * stdDev(pageViews.duration)\navg := mean(pageViews.duration)\nlongPageViews := pageViews where pageViews.duration > (avg + bound)\nlongPageViews.userId'
-    },
-    // FULLY-APPLIED FUNCTIONS
-    {
-        name : "21 Fully Applied Functions",
-        code : "pageViews := //pageViews\npageViewsForUser('userId) := pageViews where pageViews.userId = 'userId\npageViewsForUser(12345)"
-    },
-    // PARTIALLY-APPLIED FUNCTIONS, PART 1
-    {
-        name : "22 Partially Applied Function - Part 1",
-        code : "pageViews := //pageViews\npageViewsForUser('userId) :=\n\t{userId: 'userId, meanPageView: mean(pageViews.duration where pageViews.userId = 'userId)}\npageViewsForUser"
-    },
-    // PARTIALLY-APPLIED FUNCTIONS, PART 2
-    {
-        name : "23 Partially Applied Function - Part 2",
-        code : "clicks := //clicks\nviews  := //views\nclickthroughRate('page) := \n\t{page: 'page, ctr: count(clicks where clicks.pageId = 'page) / count(views where views.pageId = 'page)}\nclickthroughRate"
-    },
-    // AUGMENTATION
-    {
-        name : "24 Augmentation",
-        code : 'clicks := //clicks\nclicksWithDays := clicks with {day: (std::time::dayOfYear(clicks.timeString)) }\nclicksWithDays'
-    },
-    // JOINS
-    {
-        name : "25 Joins",
-        code : "customers := //customers\norders := //orders\nfractionForUser('userId) :=\n\tcustomers' := customers where customers.userId = 'userId\n\torders' := orders where orders.userId = 'userId\n\tcustomers' ~ orders'\n\torders.total / customers.income\nfractionForUser"
-    },
-    // SELF JOINS
-    {
-        name : "26 Self-Joins",
-        code : "users  := //users\nusers' := new users\nusers ~ users'\n{ location: users.location.state,  income: users.income }"
+        name : "Movie Ratings",
+        code : '--This query takes movie ratings, creates an overall rating and returns the top x results\n\ndata := //movie_ratings -- assigns the movie_ratings data to a variable called \"data\"\nnumberOfRecommendations := 10 -- creates a variable that determines how many rankings are returned\nhighestRating := max(data.rating) --the rating of the highest rated movie, for use below \n\nrecommendation := forall \'movie --will apply the following code  to each movie\n{movie: \'movie, overallRating: (mean(data.rating where data.movie = \'movie)- stdDev(data.rating where data.movie = \'movie))+ (min(data.rating where data.movie = \'movie)/highestRating)}\n--The ratings are composed by taking the average rating, subtracting the standard deviation (to reward consistent movies) and adding the ratio of the minimum rating over the highest possible rating (to reward movies that are not disliked by anyone)\n\nrank := std::stats::rank(recommendation.overallRating) -- ranks the overall movie ratings and stores it in \'rank\'\nmaxRank := max(rank) -- the highest rank (higher ranks are higher numbers, i.e. 1st is the lowest rank)\n\nrecommendation where rank > maxRank - numberOfRecommendations'
+    
     }];
 
     var map = {};
