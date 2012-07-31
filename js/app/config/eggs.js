17d2d1a5d720c42e9cc255109b6da5de7b541f82
Matthew T De Goes <matthew@reportgrid.com>
2012-07-31 13:01:20 -0600

    IDE Update
    Egg Addition and Logo Update to Lab

--- js/app/config/eggs.js
+++ js/app/config/eggs.js
@@ -6,10 +6,35 @@ function(utils) {
         question : "who did this?",
         answer : 'Franco Ponticelli ... the amazing programmer'
     }
-    /*
     , {
+     question : "how many zombies does it take to screw in a lightbulb?",
+     answer : '1 precog zombie, and as many minions an nessecary'
+    }
+    /*, {
+     question : "who is Matthew?",
+     answer : 'BOB'
+    }, {
+     question : "who is Matthew?",
+     answer : 'BOB'
+    }, {
+     question : "who is Matthew?",
+     answer : 'BOB'
+    }, {
+     question : "who is Matthew?",
+     answer : 'BOB'
+    }, {
+     question : "who is Matthew?",
+     answer : 'BOB'
+    }, {
+     question : "who is Matthew?",
+     answer : 'BOB'
+    }, {
+     question : "who is Matthew?",
+     answer : 'BOB'
+    }, {
      question : "who is Matthew?",
      answer : 'BOB'
     }
+    
     */];
 });
