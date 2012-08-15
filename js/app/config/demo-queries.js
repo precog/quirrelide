1d6ffed0e18a815c4c7476d9bc79e116a486d646
Nathan <nathanlubchenco@Nathans-MacBook-Air.local>
2012-08-15 11:35:33 -0600

    add medal queries

--- js/app/config/demo-queries.js
+++ js/app/config/demo-queries.js
@@ -4,25 +4,37 @@ define([
 
 function(utils) {
     var values = [{
-        name : "examples/summer_games/Athletes By Country",
+        name : "examples/summer games medals/Medals By Country",
+        code : "data := //summer_games/londonMedals\nbyCountry := forall 'Country\n  {country: 'Country,\n   total: sum(data.Total where data.Country = 'Country)}\nbyCountry"
+    }, {
+        name : "examples/summer games medals/Attributes By Sport",
+        code : "data := //summer_games/londonMedals\nbySport := forall 'Sport \n  {sport: 'Sport, \n   aveAge: mean(data.Age where data.Sport = 'Sport),\n   aveWeight: mean(data.Weight where data.Sport = 'Sport),\n   aveHeight: mean(data.HeightIncm where data.Sport = 'Sport)}\nbySport"
+    }, {
+        name : "examples/summer games medals/Correlation of Letters in Name and Medals",
+        code : "data := //summer_games/londonMedals\nbyCountry := forall 'Country\n  { country: 'Country,\n   gold: sum(data.G where data.Country = 'Country),\n   silver: sum(data.S where data.Country = 'Country),\n   bronze: sum(data.B where data.Country = 'Country),\n   total: sum(data.Total where data.Country = 'Country),\n   aveLettersInName: mean(std::string::length(data.Name where data.Country = 'Country))}\nstd::stats::corr(byCountry.aveLettersInName,byCountry.total)"
+    }, {
+        name : "examples/summer games medals/Percentage of Female Athletes by Country",
+        code : "data := //summer_games/londonMedals\nbyCountry := forall 'Country\n  { country: 'Country,\n   percentWomen: sum(data.Total where data.Sex = \"F\" & data.Country = 'Country)\n   /sum(data.Total where data.Country = 'Country),\n   total: sum(data.Total where data.Country = 'Country)}\nfiltered := byCountry where byCountry.total >=5\nfiltered where std::stats::rank(neg filtered.percentWomen) <= 10"
+    }, {
+        name : "examples/summer games athletes/Athletes By Country",
         code : "data := //summer_games/athletes \nforall 'Countryname \n  {CountryName: 'Countryname, count: count(data where data.Countryname = 'Countryname)}"
     }, {
-        name : "examples/summer_games/Athletes By Sport",
+        name : "examples/summer games athletes/Athletes By Sport",
         code : "data := //summer_games/athletes \nforall 'Sportname \n  {Sportname: 'Sportname, count: count(data where data.Sportname = 'Sportname)}"
     }, {
-        name : "examples/summer_games/Top US Sports - Men",
+        name : "examples/summer games athletes/Top US Sports - Men",
         code : "data := //summer_games/athletes \nUSMaleAthletes := data where data.Countryname = \"US\" & data.Sex = \"M\" \nbySport := forall 'Sportname \n  {sport: 'Sportname, numberOfAthletes: count(USMaleAthletes where USMaleAthletes.Sportname = 'Sportname)} \nrank := std::stats::rank(bySport.numberOfAthletes) \nbySport where rank > max(rank) - 10"
     }, {
-        name : "examples/summer_games/Top US Sports - Women",
+        name : "examples/summer games athletes/Top US Sports - Women",
         code : "data := //summer_games/athletes \nUSFemaleAthletes := data where data.Countryname = \"US\" & data.Sex = \"F\" \nbySport := forall 'Sportname \n  {sport: 'Sportname, numberOfAthletes: count(USFemaleAthletes where USFemaleAthletes.Sportname = 'Sportname)} \nrank := std::stats::rank(bySport.numberOfAthletes) \nbySport where rank > max(rank) - 10"
     }, {
-        name : "examples/summer_games/Percentage of Female Athletes",
+        name : "examples/summer games athletes/Percentage of Female Athletes",
         code : "data := //summer_games/athletes \npercentageFemaleAthletesByCountry := forall 'Countryname \n  {country: 'Countryname, percentFemale: 100*count(data where data.Sex = \"F\" & data.Countryname = 'Countryname)/count(data where data.Countryname = 'Countryname)}\n \nrank := std::stats::rank(percentageFemaleAthletesByCountry.percentFemale)\n \npercentageFemaleAthletesByCountry where rank > max(rank) - 5"
     }, {
-        name : "examples/summer_games/Lowest Athletes Per Million",
+        name : "examples/summer games athletes/Lowest Athletes Per Million",
         code : "data := //summer_games/athletes \nperCapitaAthletes := forall 'Countryname \n  {country: 'Countryname, athletesPerMillion: count(data)/(data.Population/1000000) where data.Countryname = 'Countryname} \ndistinctData := distinct(perCapitaAthletes) \nrank := std::stats::rank(distinctData.athletesPerMillion) \ndistinctData where rank < min(rank) + 10"
     },{
-        name : "examples/summer_games/Highest Athletes Per Million",
+        name : "examples/summer games athletes/Highest Athletes Per Million",
         code : "data := //summer_games/athletes \nperCapitaAthletes := forall 'Countryname \n  {country: 'Countryname, athletesPerMillion: count(data)/(data.Population/1000000) where data.Countryname = 'Countryname} \ndistinctData := distinct(perCapitaAthletes) \nrank := std::stats::rank(distinctData.athletesPerMillion) \ndistinctData where rank > max(rank) - 10"
     },{
         name : "examples/Income By State",
