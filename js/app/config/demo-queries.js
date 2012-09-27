7d7b0d5a9db342c895c3c92d5015d18f9b5bc32f
Nathan <nathanlubchenco@Nathans-MacBook-Air.local>
2012-09-27 17:08:47 -0600

    updated sample queries to be compatible with new release

--- js/app/config/demo-queries.js
+++ js/app/config/demo-queries.js
@@ -5,16 +5,19 @@ define([
 function(utils) {
     var values = [{
         name : "examples/summer games medals/Medals By Country",
-        code : "data := //summer_games/londonMedals\nbyCountry := solve 'Country\n  {country: 'Country,\n   total: sum(data.Total where data.Country = 'Country)}\nbyCountry"
+        code : "data := //summer_games/london_medals\nbyCountry := solve 'Country\n  {country: 'Country,\n   total: sum(data.Total where data.Country = 'Country)}\nbyCountry"
     }, {
         name : "examples/summer games medals/Attributes By Sport",
-        code : "data := //summer_games/londonMedals\nbySport := solve 'Sport \n  {sport: 'Sport, \n   aveAge: mean(data.Age where data.Sport = 'Sport),\n   aveWeight: mean(data.Weight where data.Sport = 'Sport),\n   aveHeight: mean(data.HeightIncm where data.Sport = 'Sport)}\nbySport"
+        code : "data := //summer_games/london_medals\n solve 'Sport \n  data' := data where data.Sport = 'Sport\n  {sport: 'Sport, \n  aveAge: mean(data'.Age),\n  aveWeight: mean(data'.Weight ),\n  aveHeight: mean(data'.HeightIncm)}"
     }, {
+        name : "examples/summer games medals/Age Distribution Of Medals",
+        code : "london := //summer_games/london_medals\nsolve 'age\n  {age: 'age, medalWinnersByAge: count(london where london.Age = 'age)}"
+    },{
         name : "examples/summer games medals/Correlation of Letters in Name and Medals",
-        code : "data := //summer_games/londonMedals\nbyCountry := solve 'Country\n  { country: 'Country,\n   gold: sum(data.G where data.Country = 'Country),\n   silver: sum(data.S where data.Country = 'Country),\n   bronze: sum(data.B where data.Country = 'Country),\n   total: sum(data.Total where data.Country = 'Country),\n   aveLettersInName: mean(std::string::length(data.Name where data.Country = 'Country))}\nstd::stats::corr(byCountry.aveLettersInName,byCountry.total)"
+        code : "data := //summer_games/london_medals \nbyCountry := solve 'Country\n  data' := data where data.Country = 'Country\n  {country: 'Country,\n  gold: sum(data'.G ),\n  silver: sum(data'.S ),\n  bronze: sum(data'.B ),\n  total: sum(data'.Total ),\naveLettersInName: mean(std::string::length(data'.Name))}\n\nstd::stats::corr(byCountry.aveLettersInName,byCountry.total)"
     }, {
         name : "examples/summer games medals/Percentage of Female Athletes by Country",
-        code : "data := //summer_games/londonMedals\nbyCountry := solve 'Country\n  { country: 'Country,\n   percentWomen: sum(data.Total where data.Sex = \"F\" & data.Country = 'Country)\n   /sum(data.Total where data.Country = 'Country),\n   total: sum(data.Total where data.Country = 'Country)}\nfiltered := byCountry where byCountry.total >=5\nfiltered where std::stats::rank(neg filtered.percentWomen) <= 10"
+        code : "data := //summer_games/london_medals\nbyCountry := solve 'Country\n data' := data where data.Country = 'Country\n  {country: 'Country,\n  percentWomen: sum(data'.Total where data'.Sex = \"F\")\n  /sum(data'.Total ),\n  total: sum(data'.Total)}\n\nfiltered := byCountry where byCountry.total >=5\n\nfiltered where std::stats::rank(neg filtered.percentWomen) <= 5"
     }, {
         name : "examples/summer games athletes/Athletes By Country",
         code : "data := //summer_games/athletes \nsolve 'Countryname \n  {CountryName: 'Countryname, count: count(data where data.Countryname = 'Countryname)}"
@@ -29,13 +32,7 @@ function(utils) {
         code : "data := //summer_games/athletes \nUSFemaleAthletes := data where data.Countryname = \"US\" & data.Sex = \"F\" \nbySport := solve 'Sportname \n  {sport: 'Sportname, numberOfAthletes: count(USFemaleAthletes where USFemaleAthletes.Sportname = 'Sportname)} \nrank := std::stats::rank(bySport.numberOfAthletes) \nbySport where rank > max(rank) - 10"
     }, {
         name : "examples/summer games athletes/Percentage of Female Athletes",
-        code : "data := //summer_games/athletes \npercentageFemaleAthletesByCountry := solve 'Countryname \n  {country: 'Countryname, percentFemale: 100*count(data where data.Sex = \"F\" & data.Countryname = 'Countryname)/count(data where data.Countryname = 'Countryname)}\n \nrank := std::stats::rank(percentageFemaleAthletesByCountry.percentFemale)\n \npercentageFemaleAthletesByCountry where rank > max(rank) - 5"
-    }, {
-        name : "examples/summer games athletes/Lowest Athletes Per Million",
-        code : "data := //summer_games/athletes \nperCapitaAthletes := solve 'Countryname \n  {country: 'Countryname, athletesPerMillion: count(data)/(data.Population/1000000) where data.Countryname = 'Countryname} \ndistinctData := distinct(perCapitaAthletes) \nrank := std::stats::rank(distinctData.athletesPerMillion) \ndistinctData where rank < min(rank) + 10"
-    },{
-        name : "examples/summer games athletes/Highest Athletes Per Million",
-        code : "data := //summer_games/athletes \nperCapitaAthletes := solve 'Countryname \n  {country: 'Countryname, athletesPerMillion: count(data)/(data.Population/1000000) where data.Countryname = 'Countryname} \ndistinctData := distinct(perCapitaAthletes) \nrank := std::stats::rank(distinctData.athletesPerMillion) \ndistinctData where rank > max(rank) - 10"
+        code : "data := //summer_games/athletes \npercentageFemaleAthletesByCountry := solve 'Countryname \n  {country: 'Countryname,\n  percentFemale: 100*count(data where data.Sex = \"F\" & data.Countryname = 'Countryname)/count(data where data.Countryname = 'Countryname)}\n \nrank := std::stats::rank(neg percentageFemaleAthletesByCountry.percentFemale)\n \npercentageFemaleAthletesByCountry where rank <=5"
     }];
 
     var map = {};
