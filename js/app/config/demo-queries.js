70a8cb5c66a602ca4399383ed82cca5ad9ffd4e8
Nathan <nathanlubchenco@Nathans-MacBook-Air.local>
2012-09-28 16:56:45 -0600

    added some clickstream and tutorial sample queries

--- js/app/config/demo-queries.js
+++ js/app/config/demo-queries.js
@@ -33,6 +33,24 @@ function(utils) {
     }, {
         name : "examples/summer games athletes/Percentage of Female Athletes",
         code : "data := //summer_games/athletes \npercentageFemaleAthletesByCountry := solve 'Countryname \n  {country: 'Countryname,\n  percentFemale: 100*count(data where data.Sex = \"F\" & data.Countryname = 'Countryname)/count(data where data.Countryname = 'Countryname)}\n \nrank := std::stats::rank(neg percentageFemaleAthletesByCountry.percentFemale)\n \npercentageFemaleAthletesByCountry where rank <=5"
+    }, {
+        name : "examples/tutorial/Top Five Products",
+        code : "data := //tutorial/transactions\n\nsalesByProduct := solve 'product\n  {product: 'product,\n  sales: sum(data.total where data.ApocalypseProducts = 'product)}\nrank := std::stats::rank(neg salesByProduct.sales)\n\nsalesByProduct where rank <= 5"
+    }, {
+        name : "examples/tutorial/Top Five Products Plus Other",
+        code : "data := //tutorial/transactions\n\nsalesByProduct := solve 'ApocalypseProducts\n  {product: 'ApocalypseProducts,\n  sales: sum(data.total where data.ApocalypseProducts = 'ApocalypseProducts)}\nrank := std::stats::rank(neg salesByProduct.sales)\notherProducts := {product: \"other\",sales: sum(salesByProduct.sales where rank > 5)}\n\ntop5Products := salesByProduct where rank <= 5\nallProducts := (new otherProducts) union top5Products \nallProducts"
+    }, {
+        name : "examples/tutorial/Quantity By Month",
+        code : "data := //tutorial/transactions\ntoday := 233\ndataWithMonthAndYearAndDay := data with\n  {month: std::time::monthOfYear(data.timeStamp),\n  year: std::time::year(data.timeStamp),\n  day: std::time::dayOfYear(data.timeStamp)}\n\nquantityByMonth := solve 'month\n monthData := dataWithMonthAndYearAndDay where dataWithMonthAndYearAndDay.month = 'month\n  {month : 'month,\n  quantity: sum(monthData.quantity),\n  year : monthData.year,\n  day : monthData.day}\n\ndistinct({quantity: quantityByMonth.quantity, month: quantityByMonth.month} where\n  quantityByMonth.year=2012 & quantityByMonth.day < today)"
+    }, {
+        name : "examples/tutorial/Sales By Source at a Particular Hour",
+        code : "data := //tutorial/transactions\ndataWithHour := data with {hour: std::time::hourOfDay(data.timeStamp)}\n\naveSalesByHour :=solve 'source\n  {source : 'source,\n  averageSales: sum(dataWithHour.total where dataWithHour.source ='source),\n  hour : dataWithHour.hour}\n\n distinct(aveSalesByHour where aveSalesByHour.hour = 13)"
+    }, {
+        name : "examples/clickstream/Clicks By Gender And Week",
+        code : "import std::time::*\n\n clicks := //clicks\n clicks' := clicks with {week: weekOfYear(clicks.timeStamp)}\n\nclicksByGenderAndWeek := solve 'gender, 'week\n  {week: 'week,\n  gender: 'gender,\n  clicks: count(clicks' where clicks'.customer.gender = 'gender & clicks'.week = 'week)}\n clicksByGenderAndWeek"
+    }, {
+        name : "examples/clickstream/Clicks By Referral",
+        code : "clicks := //clicks\n\nclicksByReferral := solve 'referral\n  {referral: 'referral,\n  clicks: count(clicks where clicks.marketing.referral = 'referral)}\n\nclicksByReferral"
     }];
 
     var map = {};
