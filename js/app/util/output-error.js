78096a62508e45d171ffb2f3dc41197230a41d29
Franco Ponticelli <franco.ponticelli@gmail.com>
2012-11-02 09:49:38 -0600

    updated error output to support arrays

--- js/app/util/output-error.js
+++ js/app/util/output-error.js
@@ -11,6 +11,25 @@ function() {
         return s.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     }
 
+    function formatError(error, wrapper) {
+      var message = '<p>'+(error && escapeHtml("" + error.message || error))+'</p>';
+      if("undefined" !== typeof error.lineNum) {
+        message += '<p class="pg-position">Error at line '+error.lineNum+', column '+error.colNum+'</p>';
+        message += '<pre>'+escapeHtml(error.detail)+'</pre>';
+        $(wrapper).trigger("syntaxError", { line : error.lineNum, column : error.colNum, text : error.detail });
+      }
+
+      var $message = $(message);
+
+      if("undefined" !== typeof error.lineNum) {
+        $message.find(".pg-position").click(function() {
+          $(wrapper).trigger("requestCursorChange", { line : error.lineNum, column : error.colNum });
+        })
+      }
+
+      return $message;
+    }
+
     return {
         type : "error",
         name : "Error",
@@ -18,19 +37,13 @@ function() {
         panel : function() {
             return elPanel;
         },
-        update : function(error, options, wrapper) {
-            var message = '<p>'+(error && escapeHtml("" + error.message || error))+'</p>';
-            if("undefined" !== typeof error.lineNum) {
-                message += '<p class="pg-position">Error at line '+error.lineNum+', column '+error.colNum+':</p><br>';
-                message += '<pre>'+escapeHtml(error.detail)+'</pre>';
-                $(wrapper).trigger("syntaxError", { line : error.lineNum, column : error.colNum, text : error.detail });
-            }
-            elError.html(message);
-            elError.find(".pg-position").click(function() {
-                $(wrapper).trigger("requestCursorChange", { line : error.lineNum, column : error.colNum });
-            })
+        update : function(errors, options, wrapper) {
+          elError.find("*").remove();
+          $(errors).each(function(){
+            elError.append(formatError(this, wrapper));
+          });
         }
     };
 });
 
-// "line":"//","lineNum":1,"colNum":1,"detail":"error:1: expected operator or path or expression\n  //\n   ^"}
+// [{"message":"Errors occurred compiling your query.","line":"loadaasd","lineNum":1,"colNum":1,"detail":"undefined name: loadaasd"}]
