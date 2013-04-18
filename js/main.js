31193a4322695ee94db64f2de5e5d72666c119bc
Franco Ponticelli <franco.ponticelli@gmail.com>
2013-04-18 08:38:48 -0600

    changed default analyticsService to be https

--- js/main.js
+++ js/main.js
@@ -619,7 +619,7 @@ function(config, createLayout, openAccountDialog, createEditors, createHistory, 
       openAccountDialog(precog, function() {
         if(!precog.config.apiKey) {
           precog.config.apiKey = "5CDA81E8-9817-438A-A340-F34E578E86F8";
-          precog.config.analyticsService = "http://labcoat.precog.com/";
+          precog.config.analyticsService = "https://labcoat.precog.com/";
           window.Precog.$.Http.setUseJsonp(true);
         }
         init();
